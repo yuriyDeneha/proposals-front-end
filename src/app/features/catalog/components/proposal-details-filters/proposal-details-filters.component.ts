0b5d4a18f60d3e6266ff67169cc8b020ab674f4d
Yuriy Deneha <yuriy.deneha@reyble.team>
2025-02-19 17:14:35 +0200

    Filters UI

--- src/app/features/catalog/components/proposal-details-filters/proposal-details-filters.component.ts
+++ src/app/features/catalog/components/proposal-details-filters/proposal-details-filters.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 import { DropdownFilterComponent } from '../../../../shared/components/dropdown-filter/dropdown-filter.component';
 import { SALES_PERSONS_OPTIONS } from '../../consts/sales-persons-options.const';
 import { CalendarFilterComponent } from '../../../../shared/components/calendar-filter/calendar-filter.component';
+import { ProposalClientFilterComponent } from '../proposal-client-filter/proposal-client-filter.component';
+import { ProposalLocationFilterComponent } from '../proposal-location-filter/proposal-location-filter.component';
 
 @Component({
   selector: 'app-proposal-details-filters',
-  imports: [DropdownFilterComponent, CalendarFilterComponent],
+  imports: [
+    DropdownFilterComponent,
+    CalendarFilterComponent,
+    ProposalClientFilterComponent,
+    ProposalLocationFilterComponent,
+  ],
   templateUrl: './proposal-details-filters.component.html',
   styleUrl: './proposal-details-filters.component.scss',
 })
---
+++ src/app/features/catalog/components/proposal-location-filter/proposal-location-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proposal-location-filter',
+  imports: [],
+  templateUrl: './proposal-location-filter.component.html',
+  styleUrl: './proposal-location-filter.component.scss'
+})
+export class ProposalLocationFilterComponent {
+
+}
